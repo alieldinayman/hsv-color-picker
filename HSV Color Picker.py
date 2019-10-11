c8bcada20504146ee40ec8a3c2db2ea400084127
deh410 <andre.arahawa@cromai.com>
2019-10-11 19:50:51 -0300

    check boundaries

--- HSV Color Picker.py
+++ HSV Color Picker.py
@@ -14,13 +14,41 @@ ftypes = [
     ("All files", "*.*")
 ]
 
+def check_boundaries(value, tolerance, ranges, upper_or_lower):
+    if ranges == 0:
+        # set the boundary for hue
+        boundary = 180
+    elif ranges == 1:
+        # set the boundary for saturation and value
+        boundary = 255
+
+    if(value + tolerance > boundary):
+        value = boundary
+    elif (value - tolerance < 0):
+        value = 0
+    else:
+        if upper_or_lower == 1:
+            value = value + tolerance
+        else:
+            value = value - tolerance
+    return value
+
 def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         pixel = image_hsv[y,x]
 
         #HUE, SATURATION, AND VALUE (BRIGHTNESS) RANGES. TOLERANCE COULD BE ADJUSTED.
-        upper =  np.array([pixel[0] + 10, pixel[1] + 10, pixel[2] + 40])
-        lower =  np.array([pixel[0] - 10, pixel[1] - 10, pixel[2] - 40])
+        # Set range = 0 for hue and range = 1 for saturation and brightness
+        # set upper_or_lower = 1 for upper and upper_or_lower = 0 for lower
+        hue_upper = check_boundaries(pixel[0], 10, 0, 1)
+        hue_lower = check_boundaries(pixel[0], 10, 0, 0)
+        saturation_upper = check_boundaries(pixel[1], 10, 1, 1)
+        saturation_lower = check_boundaries(pixel[1], 10, 1, 0)
+        value_upper = check_boundaries(pixel[2], 40, 1, 1)
+        value_lower = check_boundaries(pixel[2], 40, 1, 0)
+
+        upper =  np.array([hue_upper, saturation_upper, value_upper])
+        lower =  np.array([hue_lower, saturation_lower, value_lower])
         print(lower, upper)
 
         #A MONOCHROME MASK FOR GETTING A BETTER VISION OVER THE COLORS 
